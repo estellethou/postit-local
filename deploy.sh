ecc6f7bd21e38899906f2ce63a3173155effc5c6
Estelle <estelle.thou@gmail.com>
2021-01-16 16:06:38 +0100

    deploy on page gh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:estellethou/Post-it-VueJS.git master:gh-pages
+git push -f git@github.com:estellethou/postit-local.git master:gh-pages
 
 cd -
